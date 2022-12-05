5cba5f7222ed935ad829630961ca3d11ccfe2899
Christian <c.espinoza.code@gmail.com>
2022-12-05 13:06:04 -0500

    loaded event created in img component

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ImgComponent {
   @Input() imgUrl: string = ''
-  @Output()
+  @Output() loaded = new EventEmitter();
   imageDefault:string = './assets/images/default.png'
 
   imgError() {
